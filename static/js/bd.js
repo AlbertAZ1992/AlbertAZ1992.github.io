41a0169c268474a165f8235414418f7d72a2eb98
Deacyn <1379489640@qq.com>
2018-01-07 22:58:40 +0800

    add analytics

---
+++ static/js/bd.js
@@ -0,0 +1,1 @@
+var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?5c95e1f3157775903577e3815bb8cd64";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm, s);})();
